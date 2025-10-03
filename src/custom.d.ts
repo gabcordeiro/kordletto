65ccb70b7f68d74050ca76bc4d1b3bad2e47984b
Gabriel Cordeiro <61151121+gabcordeiro@users.noreply.github.com>
2025-10-03 16:28:44 -0300

    Add files via upload

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,4 +1,10 @@
+// src/custom.d.ts
 declare module "*.jpg";
 declare module "*.png";
 declare module "*.svg";
 declare module "*.ttf";
+
+// Declaração global para o Spotify Web Playback SDK
+interface Window {
+  Spotify: any;
+}
